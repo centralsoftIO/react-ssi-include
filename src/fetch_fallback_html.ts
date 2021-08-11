06e6bc91e2c55a12ef555c522f07937c7fb535bf
Dibran Isufi <d.isufi@gmail.com>
2021-08-11 15:07:06 +0200

    fix: always fetch with request headers

--- src/fetch_fallback_html.ts
+++ src/fetch_fallback_html.ts
@@ -1,18 +1,16 @@
 export function fetchFallbackHtml (url: string) {
-  let basicAuthHeader: any
+  const headers: Record<string, string> = {
+    Accept: 'text/html'
+  }
   try {
     const { origin, username, password, pathname } = new URL(url)
     if (username && password) {
       url = `${origin}${pathname}`
-      basicAuthHeader = {
-        Authorization: 'Basic ' + btoa(username + ':' + password)
-      }
+      headers.Authorization = 'Basic ' + btoa(username + ':' + password)
     }
   // eslint-disable-next-line no-empty
   // tslint:disable-next-line: no-empty
   } catch {}
   
-  return fetch(url, {
-    headers: basicAuthHeader
-  })
+  return fetch(url, { headers })
 }
